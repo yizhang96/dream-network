ed870d60e238be556de379ca3b3d57276cf8ed4f
yizhang96 <yz3549@columbia.edu>
2022-09-18 02:02:52 -0700

    Update
    updated code

--- dream.py
+++ dream.py
@@ -108,11 +108,36 @@ nxgraph = nx.relabel_nodes(nxgraph, mapping)
 d = dict(nxgraph.degree)
 
 #plot the distribution of degrees
+
+#set color gradient
+def hex_to_RGB(hex_str):
+    """ #FFFFFF -> [255,255,255]"""
+    #Pass 16 to the integer function for change of base
+    return [int(hex_str[i:i+2], 16) for i in range(1,6,2)]
+
+def get_color_gradient(c1, c2, n):
+    """
+    Given two hex colors, returns a color gradient
+    with n colors.
+    """
+    assert n > 1
+    c1_rgb = np.array(hex_to_RGB(c1))/255
+    c2_rgb = np.array(hex_to_RGB(c2))/255
+    mix_pcts = [x/(n-1) for x in range(n)]
+    rgb_colors = [((1-mix)*c1_rgb + (mix*c2_rgb)) for mix in mix_pcts]
+    return ["#" + "".join([format(int(round(val*255)), "02x") for val in item]) for item in rgb_colors]
+#define color gradient for plot
+color1 = "#23abeb"
+color2 = "#a2dffc"
+
 #sort dictionary
 d_sorted = sorted(d.items(), key=lambda kv: kv[1], reverse = True)
 d_sorted = collections.OrderedDict(d_sorted)
-plt.bar(d_sorted.keys(), d_sorted.values(), color='g')
-plt.xticks(rotation = "vertical", size = 6)
+keys = d_sorted.keys()
+values = d_sorted.values()
+plt.bar(list(keys)[0:14], list(values)[0:14], color = get_color_gradient(color1, color2, 15))
+plt.xticks(rotation = "vertical", size = 7)
+plt.title("Degree centrality of first 15 characters in dreams")
 plt.show()
 
 #calculate and print network metrics:
@@ -129,4 +154,5 @@ print('degree assortativity', "{:.3f}".format(assortativity))
 #set node size by degree centrality
 nx.draw(nxgraph, with_labels=True, 
     node_size=[math.sqrt(v+1) * 500 for v in d.values()])
+#set node color by centrality
 plt.show()
